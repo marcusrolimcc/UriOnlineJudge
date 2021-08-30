3b5808c3a14a848e05905e9bace7ea4d66215c0c
Marcus Rolim <marcusrolimcc@gmail.com>
2021-08-29 20:37:31 -0400

    Estrutura de criação da tabela do exercício Uri2604 inserida como comentário no código

--- src/src/sql/Uri2604.sql
+++ src/src/sql/Uri2604.sql
@@ -1,1 +1,25 @@
+/*
+create database uri2604
+
+use uri2604
+
+create table products(
+	
+	id int not null primary key identity(1,1),
+	name varchar(50) not null, 
+	amount int not null default 0,
+	price numeric(10,2) not null default 0
+
+)
+
+insert products(name, amount, price) values ('Two-door wardrobe', 100, 80)
+insert products(name, amount, price) values ('Dining table', 1000, 560)
+insert products(name, amount, price) values ('Towel holder', 10000, 5.50)
+insert products(name, amount, price) values ('Computer desk', 350, 100)
+insert products(name, amount, price) values ('Chair', 3000, 210.64)
+insert products(name, amount, price) values ('Single bed', 750, 99)
+
+select * from products 
+ 
+*/
 SELECT id, name FROM products WHERE price < 10 OR price > 100;
