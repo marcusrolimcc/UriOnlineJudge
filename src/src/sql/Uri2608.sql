c2c93c49b17a7c049f7b9a239bd3dca65b3372ee
Marcus Rolim <marcusrolimcc@gmail.com>
2021-08-29 21:00:18 -0400

    Solução do Uri2608 com SQL

---
+++ src/src/sql/Uri2608.sql
@@ -0,0 +1,26 @@
+/*
+create database uri2608
+
+use uri2608
+
+create table products(
+
+	id int not null primary key identity(1,1),
+	name varchar(50) not null,
+	amount int not null default 0,
+	price numeric(10,2) not null default 0
+
+)
+
+insert products(name, amount, price) values ('Two-doors wardrobe', 100, 800)
+insert products(name, amount, price) values ('Dining table', 1000, 560)
+insert products(name, amount, price) values ('Towel holder', 10000, 25.50)
+insert products(name, amount, price) values ('Computer desk', 350, 320.50)
+insert products(name, amount, price) values ('Chair', 3000, 210.64)
+insert products(name, amount, price) values ('Single bed', 750, 460)
+
+select * from products
+
+*/
+
+select max(price), min(price) from products
